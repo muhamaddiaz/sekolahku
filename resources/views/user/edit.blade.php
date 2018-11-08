37daa41a7886a47ac58dbca47c8f67a60f3ac384
farishdnt <faruzhunter@gmmail.com>
2018-11-08 21:43:54 +0700

    memperbaiki design (1)

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -8,23 +8,37 @@
 	 style="background-image: url({{asset('images/urban-event-space-model_925x.jpg')}}); background-size: 100% 100%; background-attachment: fixed">
 	 	<div class="container pt-5">
 
-	 		<div class="main-class__greet-text pb-5 pt-5 animated bounceInDown text-center">
-     			<img style="width: 30px; height: 30px" src={{asset('images/avatar/man-1.svg')}} alt="no image"/>
+	 		<div class="main-class__greet-text pb-5 pt-5 animated bounceInDown text-center" style="background-color: #000080">
                     @if(Auth::user()->role == 1)
-                     <?php $show = DB::select("SELECT * FROM users WHERE id=?",[Auth::user()->id]) ?>
-     			<h1 style="font-size: 2rem; font-weight: 600"><?php echo $show[0]->name ?></h1>
-                    <?php $data = DB::select("SELECT * FROM school_infos WHERE id=?",[$show[0]->id]) ?>
-     			<h2 style="font-size: 1.5rem; font-weight: 500"><?php echo $data[0]->school_name ?></h2>
+                         <?php $show = DB::select("SELECT * FROM users WHERE id=?",[Auth::user()->id]) ?>
+                              @if(!$show[0]->foto)
+                                   <img style="width: 100px; height: 100px" src={{asset('images/avatar/man-1.svg')}} alt="no image"/>
+                              @else
+                                   <img style="width: 100px; height: 100px" src="{{url('profile_picture')}}/{{<?php echo $show[0]->foto ?>}}" />
+                              @endif
+          			<h1 style="font-size: 2rem; font-weight: 600"><?php echo $show[0]->name ?></h1>
+                         <?php $data = DB::select("SELECT * FROM school_infos WHERE id=?",[$show[0]->id]) ?>
+          			<h2 style="font-size: 1.5rem; font-weight: 500"><?php echo $data[0]->school_name ?></h2>
                     @elseif(Auth::user()->role == 2)
-                    <?php $show = DB::select("SELECT * FROM guru WHERE id=?",[Auth::user()->id]) ?>
-                    <h1 style="font-size: 2rem; font-weight: 600"><?php echo $show[0]->nama ?></h1>
-                    <?php $data = DB::select("SELECT * FROM school_infos WHERE id=?",[$show[0]->id]) ?>
-                    <h2 style="font-size: 1.5rem; font-weight: 500"><?php echo $data[0]->school_name ?></h2>
+                         <?php $show = DB::select("SELECT * FROM guru WHERE id=?",[Auth::user()->id]) ?>
+                              @if(!$show[0]->foto)
+                                   <img style="width: 100px; height: 100px" src={{asset('images/avatar/man-1.svg')}} alt="no image"/>
+                              @else
+                                   <img style="width: 100px; height: 100px" src="{{url('profile_picture')}}/{{<?php echo $show[0]->foto ?>}}" />
+                              @endif
+                         <h1 style="font-size: 2rem; font-weight: 600"><?php echo $show[0]->nama ?></h1>
+                         <?php $data = DB::select("SELECT * FROM school_infos WHERE id=?",[$show[0]->id]) ?>
+                         <h2 style="font-size: 1.5rem; font-weight: 500"><?php echo $data[0]->school_name ?></h2>
                     @else
-                    <?php $show = DB::select("SELECT * FROM siswa WHERE id=?",[Auth::user()->id]) ?>
-                    <h1 style="font-size: 2rem; font-weight: 600"><?php echo $show[0]->nama ?></h1>
-                    <?php $data = DB::select("SELECT * FROM school_infos WHERE id=?",[$show[0]->id]) ?>
-                    <h2 style="font-size: 1.5rem; font-weight: 500"><?php echo $data[0]->school_name ?></h2>
+                         <?php $show = DB::select("SELECT * FROM siswa WHERE id=?",[Auth::user()->id]) ?>
+                              @if(!$show[0]->foto)
+                                   <img style="width: 100px; height: 100px" src={{asset('images/avatar/man-1.svg')}} alt="no image"/>
+                              @else
+                                   <img style="width: 100px; height: 100px" src="{{url('profile_picture')}}/{{<?php echo $show[0]->foto ?>}}" />
+                              @endif
+                         <h1 style="font-size: 2rem; font-weight: 600"><?php echo $show[0]->nama ?></h1>
+                         <?php $data = DB::select("SELECT * FROM school_infos WHERE id=?",[$show[0]->id]) ?>
+                         <h2 style="font-size: 1.5rem; font-weight: 500"><?php echo $data[0]->school_name ?></h2>
                     @endif
      		</div>
 
@@ -35,6 +49,7 @@
 	 			<form class="form-horizontal" action="/edit_profile/{{<?php echo $show[0]->id ?>}}" method="post" enctype="multipart/form-data">
 	 			{{ csrf_field() }}
 	 			<input type="hidden" name="id" value="<?php echo $show[0]->id ?>">
+	 			<div class="col-md-12 text-center">
 	 				<div class="form-group">
 	 					@if(!$show[0]->foto)
 	 					<img style="width: 50px; height: 50px" src={{asset('images/avatar/man-1.svg')}} alt="no-image">
@@ -52,7 +67,7 @@
 	 					<label class="control-label">Username : </label>
 	 					<input class="form-control" type="text" name="username" value="<?php echo $show[0]->username ?>">
 	 				</div>
-	 				<div class="form-group">
+		 			<div class="form-group">
 	 					<label class="control-label">Email : </label>
 	 					<input class="form-control" type="text" name="email" value="<?php echo $show[0]->email ?>">
 	 				</div>
@@ -61,9 +76,9 @@
 	 					<input class="form-control" type="password" name="password" value="<?php echo $show[0]->password ?>">
 		 			</div>
 		 			<div class="form-group">
-		 				<button type="submit" class="btn btn-raised btn-primary pull-right">Submit</button>
-                        <button type="reset" class="btn btn-raised btn-warning pull-right">Reset</button>
+		 				<button type="submit" class="btn btn-raised btn-primary pull-left">Submit</button>
 		 			</div>
+		 		</div>
 		 		</form>
 		 		<!--Guru profile edit-->
 		 		@elseif(Auth::user()->role == 2)
@@ -84,8 +99,7 @@
 	 					<input class="form-control" type="text" name="name" value="<?php echo $show[0]->nama ?>">
 	 				</div>
 	 				<div class="form-group">
-		 				<button type="submit" class="btn btn-raised btn-primary pull-right">Submit</button>
-                        <button type="reset" class="btn btn-raised btn-warning pull-right">Reset</button>
+		 				<button type="submit" class="btn btn-raised btn-primary pull-left">Submit</button>
 		 			</div>
 		 		</form>
 		 		<!--Siswa profile edit-->
@@ -111,8 +125,7 @@
 	 					<input class="form-control" type="text" name="email" value="<?php echo $show[0]->email ?>">
 	 				</div>
 	 				<div class="form-group">
-		 				<button type="submit" class="btn btn-raised btn-primary pull-right">Submit</button>
-                        <button type="reset" class="btn btn-raised btn-warning pull-right">Reset</button>
+		 				<button type="submit" class="btn btn-raised btn-primary pull-left">Submit</button>
 		 			</div>
 		 		</form>
 		 			@endif
