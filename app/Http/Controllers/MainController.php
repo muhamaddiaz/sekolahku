22a4bd7881019db23116d5db8169ec11c163ef45
olivier_12 <oliviergiroud1998@gmail.com>
2018-11-21 13:46:09 +0700

    tambahan Library

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,13 +6,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Mading;
 use App\Siswa;
+use App\Library;
+use App\School_Info;
+use App\User;
 use DB; 
 
 class MainController extends Controller
 {
     public function notification() {
         $notif = Auth::user()->notifications()->orderBy('created_at', 'desc')->get();
-        return view('menu.notification', ['notif' => $notif]);
+        return view('menu.notification', [
+                'notif' => $notif
+            ]);
     }
 
     public function notificationDelete() {
@@ -21,7 +26,18 @@ class MainController extends Controller
     }
 
     public function elibrary() {
-        return view('menu.elibrary');
+        $data = Library::all();
+        $schoolInfo = Auth::user()->schoolInfo()->first();
+        $school = Library::all('school_info_id');
+        $school_library = School_Info::find($school)->first();
+        $user_library = Library::all('user_id');
+        $user = User::find($user_library)->first();
+        return view('menu.elibrary',[
+            'data' => $data,
+            'schoolInfo' => $schoolInfo,
+            'school_library' => $school_library,
+            'user' => $user
+        ]);
     }
 
     public function emading() {
@@ -60,11 +76,13 @@ class MainController extends Controller
             $data = Auth::user()->siswa()->first();
             $user = Mading::all('siswa_id');
             $siswa = Siswa::find($user)->first();
+            $school_id = User::find($siswa)->first();
             return view('menu.emading',[
                 'mading' => $mading,
                 'siswa' => $siswa,
                 'data' => $data,
-                'school' => $school
+                'school' => $school,
+                'school_id' => $school_id
             ]);
         }
     }
